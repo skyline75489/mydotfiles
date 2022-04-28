a7ff858629898a2d4bc198c49d2727ddbf12bc67
Chester Liu <skyline75489@outlook.com>
2022-04-28 13:16:21 +0800

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,4 +1,22 @@
+local wezterm = require 'wezterm';
+
+local mykeys = {}
+for i = 1, 8 do
+  -- CTRL + number to activate that tab
+  table.insert(mykeys, {
+    key=tostring(i),
+    mods="CTRL",
+    action=wezterm.action{ActivateTab=i-1},
+  })
+  -- F1 through F8 to activate that tab
+  table.insert(mykeys, {
+    key="F" .. tostring(i),
+    action=wezterm.action{ActivateTab=i-1},
+  })
+end
+
 return {
 	color_scheme = "Dracula",
-	font_size = 16
+	font_size = 16,
+	keys = mykeys,
 }
