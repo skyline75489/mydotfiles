51880c8a29230c8237fecd216264d4d5f0b4edbd
Chester Liu <skyline75489@outlook.com>
2022-04-28 15:06:21 +0800

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -31,22 +31,47 @@ end
 local default_prog = { 'bash', '-l' }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	default_prog = { 'pwsh.exe', '-NoLogo' }
+  default_prog = { 'pwsh.exe', '-NoLogo' }
 end
 if wezterm.target_triple == "x86_64-apple-darwin" then 
-	default_prog = { 'zsh', '-l' }
+  default_prog = { 'zsh', '-l' }
 end
 
 return {
-	window_frame = {
-		font_size = 14.0,
-	},
-	colors = {
-		tab_bar = {
-		}
-	},
-	color_scheme = "Dracula",
-	font_size = 14,
-	keys = mykeys,
-	default_prog = default_prog
+  window_frame = {
+    font_size = 14.0,
+  },
+  colors = {
+    tab_bar = {
+    }
+  },
+  color_scheme = "Dracula",
+  font_size = 14,
+  keys = mykeys,
+  default_prog = default_prog,
+  mouse_bindings = {
+    -- Right click sends "woot" to the terminal
+    {
+      event={Down={streak=1, button="Right"}},
+      mods="NONE",
+      action=wezterm.action{PasteFrom="Clipboard"}
+    },
+
+    -- Change the default click behavior so that it only selects
+    -- text and doesn't open hyperlinks
+    {
+      event={Up={streak=1, button="Left"}},
+      mods="NONE",
+      action=wezterm.action{CompleteSelection="PrimarySelection"},
+    },
+
+    -- and make CTRL-Click open hyperlinks
+    {
+      event={Up={streak=1, button="Left"}},
+      mods="CTRL",
+      action="OpenLinkAtMouseCursor",
+    },
+    -- NOTE that binding only the 'Up' event can give unexpected behaviors.
+    -- Read more below on the gotcha of binding an 'Up' event only.
+  },
 }
